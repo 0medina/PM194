164444ec7d4d2121d688d4b2ba7eb2d4dbdd6670
estela medina <122041816@upq.edu.mx>
2025-06-10 15:49:00 -0600

    Update EjercicioB.js

--- Practica2/EjercicioB.js
+++ Practica2/EjercicioB.js
@@ -1,9 +1,9 @@
 const productos = [
-    {nombre: "Laptop", precio: 12000 },
-    {nombre: "Mouse", precio: 250 },
-    { nombre: "Teclado", precio: 700 },
-    { nombre: "Monitor", precio: 3000 },
-]
+    {nombre:  "Laptop", precio: 12000 },
+    {nombre:  "Mouse", precio: 250 },
+    {nombre: "Teclado", precio: 700 },
+    {nombre: "Monitor", precio: 3000 }
+];
 
 // listar productos con precio mayor a 1000 y que solo se obtengan los nombres
 const nombres = productos
