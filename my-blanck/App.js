cb352389dce941c93e68c87c4cd0cf733e77cfcc
estela medina <122041816@upq.edu.mx>
2025-06-11 19:51:14 -0600

    estados_componentes

--- my-blanck/App.js
+++ my-blanck/App.js
@@ -1,11 +1,20 @@
 /*zona 1: de importaciones*/
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button} from 'react-native';
+import React,{useState} from 'react';
 
-const Texto=(props)=>{
-  const {contenido}=props;
+const Texto=()=>{
+  const[contenido,setContenido]=useState('Hola Mundo RNative');
+  const actualizaTexto=()=>{setContenido('Estado actualizado del Text')}
   return(
-    <Text>{contenido}</Text>
+  <Text onPress={actualizaTexto}>{contenido}</Text>
+  )
+};
+const Boton=()=>{
+  const [contenido,setContenido]=useState('Chinito no quelel tlabajal')
+  const actualizarButton=()=>{setContenido('Tlabaja ya actualizado!!')}
+  return(
+    <Button title={contenido} onPress={actualizarButton} />
   )
 };
 /*zona 2: Main*/
@@ -13,13 +22,14 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Button title="Tlabaja!!"></Button>
-      <Texto contenido="Hola"></Texto>
-      <Texto contenido="Mundo"></Texto>
-      <Texto contenido="React Native"></Texto>
+      <Boton></Boton>
+      <Texto></Texto>
+      <Texto></Texto>
+      <Texto></Texto>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 /*zona 3: Estetica del screen*/
 const styles = StyleSheet.create({
